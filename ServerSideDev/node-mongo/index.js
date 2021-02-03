9c7173cb74a071a0e8743899e01686ac14af4b2c
Nathaniel Mills <wnmills3@gmail.com>
2021-02-03 14:39:40 -0500

    Removing mongodb/data

--- ServerSideDev/node-mongo/index.js
+++ ServerSideDev/node-mongo/index.js
@@ -6,26 +6,26 @@ const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
 MongoClient.connect(url, (err, client) => {
-    assert.strictEqual(err,null);
+    assert.strictEqual(err, null);
     console.log('Connected correctly to the server');
 
     const db = client.db(dbname);
-    dboper.insertDocument(db, {name: 'Vadonut', description: 'Test'},
+    dboper.insertDocument(db, { name: 'Vadonut', description: 'Test' },
         'dishes', (result) => {
-        console.log("Insert Document:\n", result.ops);
-        dboper.findDocuments(db, 'dishes', (docs) => {
-            console.log('Found Documents:\n', docs);
-            dboper.updateDocument(db, {name: 'Vadonut'},
-                {description: 'Updated Test'}, 'dishes',
-                (result) => {
-                    console.log('Updated Document:\n', result.result);
-                    dboper.findDocuments(db, 'dishes', (docs) => {
-                        console.log('Found Updated Documents:\n', docs);
-                        db.dropCollection('dishes', (result) => {
-                            console.log('Dropped Collection: ', result);
+            console.log("Insert Document:\n", result.ops);
+            dboper.findDocuments(db, 'dishes', (docs) => {
+                console.log('Found Documents:\n', docs);
+                dboper.updateDocument(db, { name: 'Vadonut' },
+                    { description: 'Updated Test' }, 'dishes',
+                    (result) => {
+                        console.log('Updated Document:\n', result.result);
+                        dboper.findDocuments(db, 'dishes', (docs) => {
+                            console.log('Found Updated Documents:\n', docs);
+                            db.dropCollection('dishes', (result) => {
+                                console.log('Dropped Collection: ', result);
+                            });
                         });
                     });
             });
         });
-    });
 });
