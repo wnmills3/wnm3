e3fd931ae70b6d033d4e05a655c2c05b194ba934
Nathaniel Mills <wnmills3@gmail.com>
2021-02-04 16:25:21 -0500

    Mongoose Part 3

--- ServerSideDev/node-mongoose/index.js
+++ ServerSideDev/node-mongoose/index.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
+
+// needed for updated version
+mongoose.set('useFindAndModify',false);
+
 const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+
+// options needed for updated version
+const connect = mongoose.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 connect.then((db) => {
     console.log('Connected correctl to server');
     // var newDish = Dishes({
@@ -16,11 +26,27 @@ connect.then((db) => {
     })
         .then((dish) => {
             console.log(dish);
-            return Dishes.find({}).exec();
+
+            return Dishes.findByIdAndUpdate(dish._id,{
+                $set: {description: 'Updated test'}
+            },{
+                new: true
+            }).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
-            return Dishes.remove({});
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
+            // remove is deprecated using deleteOne
+            return Dishes.deleteOne({});
         })
         .then(() => {
             return mongoose.connection.close();
