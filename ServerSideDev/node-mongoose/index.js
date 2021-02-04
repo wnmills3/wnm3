6f3a6cddad2610316a7927ca2425f932984352cf
Nathaniel Mills <wnmills3@gmail.com>
2021-02-04 15:53:12 -0500

    Mongoose Part 2

--- ServerSideDev/node-mongoose/index.js
+++ ServerSideDev/node-mongoose/index.js
@@ -4,24 +4,28 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctl to server');
-    var newDish = Dishes({
+    // var newDish = Dishes({
+    //     name: 'Uthappizza',
+    //     description: 'test'
+    // });
+
+    // newDish.save()
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
-
-    newDish.save()
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.find({}).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
-        return Dishes.remove({});
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .then((dish) => {
+            console.log(dish);
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
