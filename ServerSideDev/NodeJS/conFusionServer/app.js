a4047e247e26bd540ac98fad9ea944b1d87a4a6c
Nathaniel Mills <wnmills3@gmail.com>
2021-02-05 14:27:18 -0500

    Cookies

--- ServerSideDev/NodeJS/conFusionServer/app.js
+++ ServerSideDev/NodeJS/conFusionServer/app.js
@@ -43,42 +43,41 @@ app.use(cookieParser('12345-67890-09876-54321'));
 
 // add authentication before static routes
 function auth(req, res, next) {
-  console.log(req.signedCookies);
-  if (!req.signedCookies.user) {
 
+  if (!req.signedCookies.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
-      return next(err);
+      next(err);
+      return;
     }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === 'admin' && password === 'password') {
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
       res.cookie('user', 'admin', { signed: true });
       next(); // authorized
     } else {
       var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
-      return next(err);
+      next(err);
     }
-  } else {
+  }
+  else {
     if (req.signedCookies.user === 'admin') {
       next();
-    } else {
+    }
+    else {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
-      return next(err);
+      next(err);
     }
   }
 };
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
