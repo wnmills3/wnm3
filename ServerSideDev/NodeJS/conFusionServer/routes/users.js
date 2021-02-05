7e171ae364d74b5fe71eb4aebd3cf08394ee261b
Nathaniel Mills <wnmills3@gmail.com>
2021-02-05 17:48:41 -0500

    Sessions 3

--- ServerSideDev/NodeJS/conFusionServer/routes/users.js
+++ ServerSideDev/NodeJS/conFusionServer/routes/users.js
@@ -37,6 +37,7 @@ router.post('/login', passport.authenticate('local'), (req, res, next) => {
 })
 
 router.get('/logout', (req, res, next) => {
+  console.log(req.session);
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
